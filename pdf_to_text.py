46473894469f21ccd9352b902e8d5bd3e11e6d19
Xin Yan <xinyan@xins-mac-mini.mynetworksettings.com>
2023-11-02 21:04:53 -0500

    Reviewed.

--- pdf_to_text.py
+++ pdf_to_text.py
@@ -1,10 +1,5 @@
-import PyPDF2
-
-print("hello world")
-
- 
 from pdfminer.high_level import extract_text
- 
+
 text = extract_text("/Users/xinyan/Desktop/DH/Affective_Map/example.pdf")
 output_file = open("//Users/xinyan/Desktop/DH/Affective_Map/example.txt", "w")
 output_file.write(text)
