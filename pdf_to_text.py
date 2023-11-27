f8afd5ebd4f1b89358b2e3bd44d5cf816dd1e755
Zhuokai Zhao <zhuokai@uchicago.edu>
2023-11-26 23:37:35 -0600

    Added openai for sentence breaking.

--- pdf_to_text.py
+++ pdf_to_text.py
@@ -3,6 +3,20 @@ import glob
 import os
 from tqdm import tqdm
 from pdfminer.high_level import extract_text
+from openai import OpenAI
+import json
+
+client = OpenAI(api_key="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")
+
+
+# helper function that formats the request to openai
+def get_completion(prompt, model="gpt-3.5-turbo", temperature=0):
+    # def get_completion(prompt, model="gpt-4"):
+    messages = [{"role": "user", "content": prompt}]
+    response = client.chat.completions.create(
+        model=model, messages=messages, temperature=temperature
+    )
+    return response.choices[0].message.content
 
 
 def main():
@@ -44,15 +58,53 @@ def main():
         print(f"\nNumber of PDF files: {len(all_pdf_paths)}")
 
     # convert all the PDFs to texts
-    for pdf_path in tqdm(all_pdf_paths, desc="Converting PDFs to texts"):
+    for pdf_path in all_pdf_paths:
+        if verbosity:
+            print(f"\nProcessing {pdf_path}")
+
+        # check if the associated text file already exists
+        file_name = os.path.basename(pdf_path).split(".pdf")[0]
+        txt_path = os.path.join(output_dir, file_name + ".txt")
+        if os.path.exists(txt_path):
+            if verbosity:
+                print(f"Text file {txt_path} already exists")
+            continue
+
         # get the text
         text = extract_text(pdf_path)
+
         # write the text to a file
-        # get the file name
-        file_name = os.path.basename(pdf_path).split(".pdf")[0]
-        output_file = open(output_dir + file_name + ".txt", "w")
+        output_file = open(txt_path, "w")
         output_file.write(text)
         output_file.close()
+
+    # get all the PDF file path from the folder
+    all_txt_paths = glob.glob(output_dir + "*.txt")
+
+    for txt_path in all_txt_paths:
+        if verbosity:
+            print(f"\nCleaning {txt_path}")
+        # load the text
+        with open(txt_path, "r") as f:
+            text_list = f.readlines()
+        # combine into strings
+        text = "".join(text_list)
+
+        # split the text with ".\n"
+        text = text.split(".\n\n")
+        text = [t + "." for t in text]
+        cleaned_text = ""
+        for cur_text in tqdm(text, desc="Cleaning text"):
+            prompt = f"""
+                Break the paragraphs delimited by triple backticks into sentences. Separate each sentence with an empty line. '''{cur_text}'''
+            """
+            cur_response = get_completion(prompt)
+            cleaned_text += cur_response + "\n"
+
+        # save the cleaned text
+        clean_txt_path = txt_path.replace(".txt", "_clean.txt")
+        with open(clean_txt_path, "w") as f:
+            f.writelines(cleaned_text)
 
 
 if __name__ == "__main__":
